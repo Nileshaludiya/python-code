28d7f279f2b817dd556a93d3b599f758771becf3
Nileshaludiya <nileshaludiya@gmail.com>
2025-03-24 04:51:43 -0700

    Update tutorial58.py

--- tutorial58.py
+++ tutorial58.py
@@ -1,3 +1,10 @@
+# "Capitalize the First Letter of Each Word
+# Problem: Given a sentence, capitalize the first letter of each word.
+# Example:
+
+# Input: ""hello world""
+# Output: ""Hello World"""	
+
 def first_letter_capital(text):
     return text.title()
 
