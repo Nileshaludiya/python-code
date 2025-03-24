1789507a0c8bbee6bc1dd4f6ea4ccbd1a70a1202
Nileshaludiya <nileshaludiya@gmail.com>
2025-03-24 10:07:47 -0700

    Update tutorial65.py

--- tutorial65.py
+++ tutorial65.py
@@ -1,3 +1,9 @@
+# "Check if One String is a Substring of Another
+# Problem: Given two strings s1 and s2, check if s2 is a substring of s1.
+# Example:
+# Input: s1 = ""hello world"", s2 = ""world""
+# Output: True"
+
 def search_substring(s,s1):
      return s1 in s           
 str = input("Enter a string: ")
