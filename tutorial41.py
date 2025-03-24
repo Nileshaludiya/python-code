9732696bed5ff024d81aad71ed06415e5c3af56c
Nileshaludiya <nileshaludiya@gmail.com>
2025-03-24 04:42:56 -0700

    Update tutorial41.py

--- tutorial41.py
+++ tutorial41.py
@@ -1,3 +1,10 @@
+# Problem:
+# Use division by 2 to check if a number is even or odd.
+
+# Example:
+# Input: n = 27
+# Output: Odd
+
 num = int(input("Enter a value: "))
 if (num % 2 == 0): 
     print(f"The value {num} is even")
