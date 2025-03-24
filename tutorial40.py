25cb7afb6ffca66cafeb3a9856b52a158536d0bc
Nileshaludiya <nileshaludiya@gmail.com>
2025-03-24 04:41:28 -0700

    Update tutorial40.py

--- tutorial40.py
+++ tutorial40.py
@@ -1,3 +1,11 @@
+
+# Remove a Specific Digit from a Number
+# Problem: Given a number N and a digit D, remove all occurrences of D from N.
+# Example:
+
+# Input:  N = 10501, D = 0  
+# Output: 151  
+
 N = int(input("Enter a value of N: "))
 D = int(input("Enter the value you want to remove: "))
 B = " "
