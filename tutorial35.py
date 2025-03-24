0091f0f63c52a7b4de131b385087fd06408bc120
Nileshaludiya <nileshaludiya@gmail.com>
2025-03-24 04:36:46 -0700

    Update tutorial35.py

--- tutorial35.py
+++ tutorial35.py
@@ -1,3 +1,8 @@
+# You are given an integer A, you need to find and return	
+	
+# the sum of all the even numbers between 1and A.	
+# Even numbers are those numbers that are divisible by 2.	
+    
 A = int(input("Enter value of A: "))
 reversed = 0
 for i in range(1,A+1):
