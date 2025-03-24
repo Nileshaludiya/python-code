1a6d409de828a8b798c19af8d66b1dfe521bb13f
Nileshaludiya <nileshaludiya@gmail.com>
2025-03-24 04:37:51 -0700

    Update tutorial37.py

--- tutorial37.py
+++ tutorial37.py
@@ -1,3 +1,7 @@
+# "Write a program to print the multiplication table of the number entered by the user, N.
+
+# The table for input N = 5 should get displayed in the following form:"	
+
 def table_number(N):
     for i in range(1,11):
         mul = N*i
