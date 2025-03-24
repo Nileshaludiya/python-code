e98cc82c2d92d9a8cf96a421f595885ded09f550
Nileshaludiya <nileshaludiya@gmail.com>
2025-03-24 04:45:38 -0700

    Update tutorial45.py

--- tutorial45.py
+++ tutorial45.py
@@ -1,3 +1,11 @@
+# "The parity of a number is even if it has an even number of 1s in its binary representation and odd otherwise.
+
+# Write a function to find the parity of a number using division by 2.
+
+# Example:
+# Input: n = 9 (Binary: 1001, has 2 ones)
+# Output: Even Parity"		
+
 def decimal_to_binary(num):
     binary_number = ""
     a = 0
