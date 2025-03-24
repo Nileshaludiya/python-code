20dbfe23f77a0364a2f2e1a40827168eea5f29af
Nileshaludiya <nileshaludiya@gmail.com>
2025-03-24 04:33:19 -0700

    Update tutorial31.py

--- tutorial31.py
+++ tutorial31.py
@@ -1,3 +1,5 @@
+# Write a program to input three numbers(A, B & C) from user and print the minimum element among A, B & C.		
+
 n1 = int(input("Enter first number: "))
 n2 = int(input("Enter second number: "))
 n3 = int(input("Enter third number: "))
