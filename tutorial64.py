8b93271054ec25b5d9f857b72ad5cc49b4bcf91b
Nileshaludiya <nileshaludiya@gmail.com>
2025-03-28 05:18:10 -0700

    Update tutorial64.py

--- tutorial64.py
+++ tutorial64.py
@@ -1,3 +1,9 @@
+# "Find All Substrings of a String
+# Problem: Given a string, print all possible substrings.
+# Example:
+# Input: ""abc""
+# Output: ""a"", ""ab"", ""abc"", ""b"", ""bc"", ""c"""
+
 def find_substring(s):
     for i in range(len(s)):
         for j in range(i+1,len(s)+1):
