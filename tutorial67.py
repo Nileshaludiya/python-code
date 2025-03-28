63e7929411f466fedecde84d5dcba37f19d3894d
Nileshaludiya <nileshaludiya@gmail.com>
2025-03-28 05:19:40 -0700

    Update tutorial67.py

--- tutorial67.py
+++ tutorial67.py
@@ -1,3 +1,9 @@
+# "Find the First Non-Repeating Character
+# Problem: Given a string, find the first character that does not repeat.
+# Example:
+# Input: ""aabbccdeff""
+# Output: ""d"""
+
 def first_non_repeating_letter(s):
     for i in s:
         if s.count(i) == 1:
