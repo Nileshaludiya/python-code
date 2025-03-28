f9bb9821957ef900ca02927cb213db701376a0aa
Nileshaludiya <nileshaludiya@gmail.com>
2025-03-28 05:29:57 -0700

    Update tutorial63.py

--- tutorial63.py
+++ tutorial63.py
@@ -1,3 +1,11 @@
+# "Convert a String to an Integer
+# Problem: Convert a numeric string into an integer without using int().
+# Example:
+
+# Input: ""1234""
+# Output: 1234
+# "
+
 def convert_str_int(str1):
     print(type(str1))
     integer = int(str1)
