5cffbc43fc0287b0413743798085c2d17f0eecb7
Nileshaludiya <nileshaludiya@gmail.com>
2025-03-28 05:25:32 -0700

    Update tutorial62.py

--- tutorial62.py
+++ tutorial62.py
@@ -1,3 +1,11 @@
+# "Count the Frequency of Each Character
+# Problem: Given a string, count how often each character appears.
+# Example:
+
+# Input: ""apple""
+# Output: {'a': 1, 'p': 2, 'l': 1, 'e': 1}
+# "
+
 def count_letter(text):
     file = {}
     for x in text:
